3f3cc224b663197b56254af1dfa0bf56ec8e968d
unknown <tatlidilonur0@gmail.com>
2024-02-04 14:37:13 -0500

    java script codes added for contact

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -47,3 +47,17 @@ function disableButton(btnvalue) {
         document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
     }
 }
+    //listen to user input code
+    var codebox = document.getElementById("codeentered"); //get textbox
+    codebox.addEventListener("input", evaluateCode); //listen to code entered in textbox
+
+    //run function if detected user had entered a character in textbox
+    function evaluateCode() {
+        getCode = document.getElementById("codeentered").value; //get character entered
+        var charset1 = getCode.trim(); //remove any hidden characters entered
+        var charset2 = code.trim(); //remove any hidden characters generated
+        //test if code entered matches the number of generated characters
+        if (charset1.length == charset2.length && charset1 == charset2) {
+            disableButton(false); //if match, run the function to enable button
+        }
+    }
