2d3ec522c1645b40fd7887a27845e928f665d215
unknown <tatlidilonur0@gmail.com>
2024-02-04 14:20:39 -0500

    adding button properties to contacts JS

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -25,3 +25,25 @@ function disableButton() {
 
 //activate function
 disableButton();
+
+
+
+//NOTE: submit button is initially disabled upon loading of this page - see <body> in html
+
+var code = ''; //to store generated codes and initialize to empty value
+var getCode = ''; //to store entered code
+var btnvalue; //for button boolean value
+//create variable to hold the type of characters we want to show as codes
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue; //able/disable button
+    if (btnvalue === true) { //test if button is disabled or enabled
+        //set button and label color translucent
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        //set button and label color with no transparency
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
+}
