3328649ea74877e0acb0c41525d33fd30bfe0b27
unknown <tatlidilonur0@gmail.com>
2024-02-04 01:52:56 -0500

    random js added

---
+++ js/randomcodes.js
@@ -0,0 +1,16 @@
+/*---------random codes----------*/
+
+//function to generate combination of characters
+function generateCode(){
+     //Create variables to store generated codes and the type of characters we want to show as codes
+     var code = ''; //initialize to null value
+     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+     // generate m,ultiple characters using loop
+     for (i=1; i<=8; i++){
+        var char = Math.random() * str.length; //random select a character from the vari
+        code += str.charAt(Math.floor(char)); //accumulate the generated character into a string of 8
+        
+     }
+     return code; //return the final accumulated strring when loop ends
+}
