060452386ee5a9ed2405be71d40989a2f435087a
unknown <tatlidilonur0@gmail.com>
2024-02-10 13:54:08 -0500

    selected activities to enquire are added

--- js/activity.js
+++ js/activity.js
@@ -1,10 +1,23 @@
-$(document).ready(function(){
-    // Target only cells within the tbody to exclude header cells
-    $('table tbody tr td').not(':contains("Not Available")').addClass('selectable');
+//User interaction with table cells
+$(document).ready(function() {
+    $("td").click(function() { // user select a table data cell
+        var content = $(this).text(); // get content of cell
 
-    // Add click event listener to selectable cells
-    $('.selectable').click(function() {
-        // Toggle the highlighted class to add/remove the highlight
-        $(this).toggleClass('highlighted');
+        if (content != "Not Available") { // check if content does not contain a particular string
+            $(this).toggleClass("highlighted"); // add or remove class when cell is selected
+
+            if ($(this).hasClass("highlighted")) { // check if selected cell has class
+                $('#displaySelected').css("visibility", "visible"); // make display box visible
+                $('#displaySelected').css("margin-top", "2em"); // add spaces above display box
+                $('#result').append("<p>"+content+"</p>"); // add child element with contents of cell
+            } else { //if selected cell don't have class
+                $('#result p:contains('+content+')').remove(); // remove child element
+            }
+        }
+
+        if ($('#result').has('p').length == false) { // check if there are any child elements within parent
+            $('#displaySelected').css("visibility", "hidden"); // make display box hidden
+            $('#displaySelected').css("margin-top", "0"); // remove spaces above display box
+        }
     });
 });
